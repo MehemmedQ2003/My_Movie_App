4b7abb861a449adad2897940acf43cf599df90e3
mehemmed qardasov <116949722+MehemmedQ2003@users.noreply.github.com>
2024-11-16 13:53:58 +0400

    Movie App - Map function

---
+++ src/components/App.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+// import SearchBar from "./SearchBar";
+import MovieList from "./MovieList";
+
+class App extends React.Component {
+  state = {
+    movies: [
+      {
+        id: 1,
+        name: "Peygamber(s.a.s)",
+        rating: 10,
+        overview: "This film was very well received in the Islamic community",
+        imageUrl: "https://www.hdfilmizle.to/v/15122/poster/watch/hz-muhammed-allahin-elcisi.jpg",
+      },
+      {
+        id: 2,
+        name: "Hz Omer",
+        rating: 9.5,
+        overview: "This film was very well received in the Islamic community",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/tr/d/d2/Omar_%28TV_Series%29.jpg",
+      },
+      {
+        id: 3,
+        name: "Caghri",
+        rating: 9,
+        overview: "This film was very well received in the Islamic community",
+        imageUrl: "https://tr.web.img3.acsta.net/pictures/22/03/16/14/34/3489890.jpg",
+      }
+    ]
+  }
+  render(){
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            {/* <SearchBar /> */}
+          </div>
+        </div>
+        <MovieList movies={this.state.movies}/>
+      </div>
+    )
+  }
+};
+
+export default App;
