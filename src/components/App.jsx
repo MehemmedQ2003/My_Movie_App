03622e4ed2b8d9f8ca89dad4646fd37a8ae2f079
mehemmed qardasov <116949722+MehemmedQ2003@users.noreply.github.com>
2024-11-18 20:04:51 +0400

    Movie App - Fake Rest Api, Fetch, Async, Await

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,53 +4,19 @@ import MovieList from "./MovieList";
 
 class App extends React.Component {
   state = {
-    movies: [
-      {
-        id: 1,
-        name: "Peygamber(s.a.s)",
-        rating: 10,
-        overview: "This film was very well received in the Islamic community",
-        imageUrl: "https://www.hdfilmizle.to/v/15122/poster/watch/hz-muhammed-allahin-elcisi.jpg",
-      },
-      {
-        id: 2,
-        name: "Hz Omer",
-        rating: 9.5,
-        overview: "This film was very well received in the Islamic community",
-        imageUrl: "https://upload.wikimedia.org/wikipedia/tr/d/d2/Omar_%28TV_Series%29.jpg",
-      },
-      {
-        id: 3,
-        name: "Caghri",
-        rating: 9,
-        overview: "This film was very well received in the Islamic community",
-        imageUrl: "https://tr.web.img3.acsta.net/pictures/22/03/16/14/34/3489890.jpg",
-      },
-      {
-        id: 4,
-        name: "Fetih 1453",
-        rating: 8.5,
-        overview: "A historical film about the conquest of Constantinople by Sultan Mehmed II.",
-        imageUrl: "https://upload.wikimedia.org/wikipedia/az/e/eb/F%C9%99th_1453_%28film%2C_2012%29.jpg",
-      },
-      {
-        id: 5,
-        name: "Alparslan Great Seljuks",
-        rating: 9.2,
-        overview: "A classic film depicting the life and times of Prophet Muhammad (s.a.s).",
-        imageUrl: "https://m.media-amazon.com/images/I/71qMA0YsRwL.jpg",
-      },
-      {
-        id: 6,
-        name: "Ertugrul",
-        rating: 9.7,
-        overview: "A popular series about the life of Ertuğrul Bey, the father of Osman I, founder of the Ottoman Empire.",
-        imageUrl: "https://m.media-amazon.com/images/I/61aiMK0teML._UF1000,1000_QL80_.jpg",
-      }      
-    ],
+    movies: [],
 
     searchQuery:""
   };
+
+  async componentDidMount(){
+    const baseUrl = "http://localhost:5174/movies";
+    const response = await fetch(baseUrl);
+    console.log(response);
+    const data = await response.json();
+    console.log(data)
+    this.setState({movies: data})
+  }
 
   deleteMovie = (movie) => {
     const newMovieList = this.state.movies.filter(
